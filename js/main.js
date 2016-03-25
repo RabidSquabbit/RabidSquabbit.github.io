aea437bc3ead714b3885387adb60fd6e505703d7
RabidSquabbit <rabidsquabbit@gmail.com>
2016-03-25 12:53:32 -0500

    Enter Keypress

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ var myCodeMirror;
 jQuery(function() {
     myCodeMirror = CodeMirror.fromTextArea(rconConsole);
 
-
+$(document).keypress(function(e) {
+  if(e.which == 13) {
+     dewRcon.send(jQuery("#rconCommand").val());
+        jQuery("#rconCommand").val("");
+  }
+});
     //TODO: Support up arrow, enter, etc
     $("#runCommand").click(function() {
         dewRcon.send(jQuery("#rconCommand").val());
